8c13e953ec0da3a73b1711020aa53e5ca5f53593
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-02-02 06:13:04 +0530

    Add files via upload

---
+++ propspringsecurity/src/main/java/com/sohamglobal/propspringsecurity/PropspringsecurityApplication.java
@@ -0,0 +1,14 @@
+package com.sohamglobal.propspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropspringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropspringsecurityApplication.class, args);
+		System.out.println("security project running");
+	}
+
+}
