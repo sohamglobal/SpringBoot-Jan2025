0f36b56a761e167ce067badc71c664bef6e35635
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-02-02 06:12:30 +0530

    Add files via upload

---
+++ autospringsecurity/src/main/java/com/sohamglobal/autospringsecurity/AutospringsecurityApplication.java
@@ -0,0 +1,14 @@
+package com.sohamglobal.autospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutospringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutospringsecurityApplication.class, args);
+		System.out.println("auto generated spring security done...");
+	}
+
+}
